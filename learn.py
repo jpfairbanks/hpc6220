32a9497b70db4b1fa71cea67faddcbd395bec06f
James Fairbanks <james.fairbanks@gatech.edu>
2013-03-17 18:44:50 -0400

    moving stuff around and generalizing

--- learn.py
+++ learn.py
@@ -2,7 +2,10 @@ import numpy as np
 import multiprocessing as multp
 from multiprocessing import Pool as Pool
 import time
+N = 80000
 MAXCPUS = 8
+STRIDE  = 3
+CPUS = np.array([4,8])
 def pf(s):
     """@todo: Docstring for pf
 
@@ -11,28 +14,32 @@ def pf(s):
 
     """
     #print('hello %s'%s) 
-    val=0
-    for i in range(5):
-        #print('%d iter %d' %(s,i))
-        x = np.arange(1000)
-        xsq = np.sqrt(x)
-        val = (val+sum(xsq))/2
-        #print ('%d has val:%f'%(s, val))
+    val= 0
+    n  = 5000
+    #print('%d iter %d' %(s,i))
+    x = np.arange(n)
+    xsq = np.sqrt(x)
+    val = (val+sum(xsq))/n
+    #print ('%d has val:%f'%(s, val))
     return (s,val)
-times = np.zeros(MAXCPUS/2)
-for CPUS in range(0,MAXCPUS,2):
-    pool = Pool(CPUS+1)
+
+times = np.zeros(CPUS.shape)
+for i, CPUC in enumerate(CPUS):
+    pool = Pool(CPUC+1)
     ts = time.time()
-    result = pool.map_async(pf, np.arange(8000), chunksize=10)
+    result = pool.map_async(pf, np.arange(N), 
+            chunksize=N/num_chunks)
     pool.close()
     pool.join()
     te = time.time()-ts
-    times[(CPUS)/2] = te
+    times[i] = te
     print("success: %s" % result.successful())
     #print("AsyncResult: ", result.get())
 print(times)
-scala = np.log(times * (np.arange(MAXCPUS/2)+1))
+scala = np.log(times * (CPUS))
 print(scala)
 speedup = times[0]/times[-1]
-print("speedup on %d cores: %f"%(MAXCPUS, speedup))
-print("chunksize must be tuned for optimal performance, it is not enough to go big") 
+print("speedup from %d to %d cores: %f"%
+        (CPUS[0], CPUS[-1], speedup))
+print("chunksize must be tuned for optimal performance,
+       it is not enough to go big") 
