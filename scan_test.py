b866d6267f07b30180429bf501150c195fe63f31
James Fairbanks <james.fairbanks@gatech.edu>
2013-03-25 07:54:27 -0400

    removing the tree scan because it is a lie

--- scan_test.py
+++ scan_test.py
@@ -21,15 +21,6 @@ def flat_main(pool, num_procs):
         initial.extend(s)
     return initial
 
-def tree_main(pool, num_procs):
-    """Using multiproccessing to see if it is faster
-    Try using the recursive tree approach to the reduction.
-    :returns: The sum over SEQ
-
-    """
-    answers = SEQ
-    count = parutils.reduction_tree(pool, parutils.star_add, answers, NP)
-    return count
 
 def serial_main():
     """Does the main without any parallel overhead
@@ -41,18 +32,20 @@ def serial_main():
 
 if __name__ == '__main__':
     # Setup
-
     timer = timedict()
     args = par_args.get_args()
     scale = args.scale
     NP = args.procs
-    par_name = 'Tree'+str(NP)
+    if args.verbose:
+        if args.tree:
+            print('no tree method for scans because they are too slow')
     flat_name = 'Flat'+str(NP)
     timer.tic('spawning pool')
     pool = multiprocessing.Pool(processes=NP)
     timer.toc('spawning pool')
     SEQ = random.random_integers(0,1,2**scale)
-    print(SEQ)
+    if args.verbose:
+        print(SEQ)
 
 
     # Testing
@@ -62,13 +55,6 @@ if __name__ == '__main__':
     timer.toc(0)
     print('serial')
 
-    timer.tic(par_name)
-    tree_count = -1
-    if args.tree:
-        tree_count = tree_main(pool, NP)[0]
-    timer.toc(par_name)
-    print('tree')
-
     timer.tic(flat_name)
     np_count = flat_main(pool, NP)
     timer.toc(flat_name)
@@ -77,9 +63,7 @@ if __name__ == '__main__':
     # Reporting
 
     #print('sum:%s,%s,%s' % (count, tree_count, np_count))
-    print(np_count == count)
+    print('Same result: %s' % (np_count == count))
     print(repr(timer.ends))
-    print('Tree speedup: %f' %
-	    (timer.ends[0]/timer.ends[par_name]))
     print('Flat speedup: %f' %
 	    (timer.ends[0]/timer.ends[flat_name]))
