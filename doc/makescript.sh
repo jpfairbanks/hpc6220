acf7af75b37b3f7901223761df6c6565d3e0f18b
James Fairbanks <james.fairbanks@gatech.edu>
2013-04-24 10:03:17 -0400

    code highlighting and article class

--- doc/makescript.sh
+++ doc/makescript.sh
@@ -1,3 +1,6 @@
-pandoc -o writeup.tex -t latex writeup.md -V documentclass=IEEEtran --standalone
+pandoc -o writeup.tex -t latex writeup.md \
+       -V documentclass=article --standalone \
+       --highlight-style=pygments 
+       
 pdflatex writeup.tex
-evince writeup.pdf
+#evince writeup.pdf
